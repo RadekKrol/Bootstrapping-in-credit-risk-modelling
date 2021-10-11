2d8b81fff93467669f1d1786f0ee0790eeaa1418
Radoslaw Krol <radkro97@gmail.com>
2021-10-12 00:46:38 +0200

    ADD: script with used R packages

---
+++ pakiety.R
@@ -0,0 +1,36 @@
+if(!require(R6)){install.packages("R6")  
+    library(R6)}
+
+if(!require(readr)){install.packages("readr")  
+    library(readr)}
+
+if(!require(tidyverse)){install.packages("tidyverse")  
+    library(tidyverse)}
+
+if(!require(caret)){install.packages("caret")  
+    library(caret)}
+
+if(!require(MASS)){install.packages("MASS")  
+    library(MASS)}
+
+if(!require(ModelMetrics)){install.packages("ModelMetrics")  
+    library(ModelMetrics)}
+
+if(!require(assertive)){install.packages("assertive") 
+    library(assertive)
+}
+if(!require(ggplot2)){install.packages("ggplot2") 
+    library(ggplot2)
+}
+if(!require(ROCit)){install.packages("ROCit") 
+    library(ROCit)
+}
+if(!require(ggthemes)){install.packages("ggthemes") 
+    library(ggthemes)
+}
+if(!require(magrittr )){install.packages("magrittr") 
+    library(magrittr )
+}
+if(!require(reshape2 )){install.packages("reshape2") 
+    library(reshape2 )
+}
